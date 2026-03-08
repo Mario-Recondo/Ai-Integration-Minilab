4fecbd1312a04b1ef4a7804813af53e5bdac5583
Leveler96 <leveler96@gmail.com>
2026-03-08 16:56:02 -0400

    initial commit

---
+++ AiBot.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from google import genai
+from google.genai import types, errors
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+print(api_key)
+
+st.title("AI Document Assistant")
+
+if "client" not in st.session_state:
+    st.session_state.client = genai.Client(api_key=api_key)
+
+with st.sidebar:
+    st.header("Setup")
+    uploaded_file = st.file_uploader("Upload your document", type=["pdf", "txt"])
+
+    if uploaded_file and "doc_ref" not in st.session_state:
+        with st.spinner("Uploading your document..."):
+            # Check file type
+            mime_type = uploaded_file.type
+
+            # wiring the bytes to a temporary file
+            with open("temp_doc", "wb") as f:  # created a temp_doc file with writing capabilities
+                f.write(uploaded_file.getbuffer())
+
+            doc_ref = st.session_state.client.files.upload(
+                file="temp_doc",
+                config={"mime_type": mime_type}
+            )
+            st.session_state.doc_ref = doc_ref
+
+            st.session_state.chat = st.session_state.client.chats.create(
+                model="gemini-3-flash-preview",
+                config=types.GenerateContentConfig(
+                    system_instruction="You are a document expert." "Answer questions ONLY using the uploaded files."
+                                       "If the answer isn't there, say you dont know."
+                )
+            )
+
+        st.success("Document uploaded successfully!")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:  # iterate through list of dictionaries
+    with st.chat_message(msg["role"]):  # create a div block for each row
+        st.markdown(msg["content"])
+
+prompt = st.chat_input("Ask a question about the uploaded document:")
+if prompt:
+    if "chat" not in st.session_state or "doc_ref" not in st.session_state:
+        st.error("Please upload a document first!")
+    else:
+        st.session_state.messages.append({
+            "role": "user",
+            "content": prompt
+        })
+
+        with st.chat_message("user", avatar="😎"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            try:
+                response = st.session_state.chat.send_message(
+                    message=[st.session_state.doc_ref, prompt]
+                )
+                st.markdown(response.text)
+
+                st.session_state.messages.append({"role": "assistant",
+                                                  "content": response.text})
+            except errors.ServerError as e:
+                st.error("The AI model is temporarily unavailable due to high demand. please try again.")
